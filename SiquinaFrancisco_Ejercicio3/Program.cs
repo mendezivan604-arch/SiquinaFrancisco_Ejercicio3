cd11ba5a8cb1f2f1b4aed2b2661e315fc818424d
Ivan mendez <mendezivan604@gmail.com>
2026-03-07 20:59:33 -0600

    Menu y entradas

--- SiquinaFrancisco_Ejercicio3/Program.cs
+++ SiquinaFrancisco_Ejercicio3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int tipoCliente;
+int metodoPago;
+int antifraude;
+
+double montoBase;
+double descuento = 0;
+double recargo = 0;
+
+string tieneCupon;
+string codigoCupon = "";
+
+Console.WriteLine("Tipo de cliente:");
+Console.WriteLine("1 Estudiante");
+Console.WriteLine("2 Docente");
+Console.WriteLine("3 Administrativo");
+Console.WriteLine("4 Externo");
+tipoCliente = int.Parse(Console.ReadLine());
+
+Console.Write("Monto base: ");
+montoBase = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Metodo de pago:");
+Console.WriteLine("1 Efectivo");
+Console.WriteLine("2 Tarjeta");
+Console.WriteLine("3 Transferencia");
+metodoPago = int.Parse(Console.ReadLine());
+
+Console.Write("Tiene cupon (S/N): ");
+tieneCupon = Console.ReadLine();
